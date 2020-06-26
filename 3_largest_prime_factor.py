50fdebd09ec7e9b0fed9a01cca8ce9df2d6c5f6d
Wilson <wgm4143@gmail.com>
2020-06-26 04:42:16 -0400

    Added problem 3

---
+++ 3_largest_prime_factor.py
@@ -0,0 +1,44 @@
+# Problem 3
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number n?
+
+# Inputs
+# 2
+# 10
+# 17
+
+# Outputs
+# 5
+# 17
+
+t = input('') # Get number of tests
+ns = []
+for i in range(t):
+    ns.append(int(input(''))) # Get n
+
+for n in ns:
+    largestPrime = 1
+    # Start with first 'prime' to test, 2
+    p=2 
+    # Loop through 'primes' until the p we are testing is greater than the square root of n
+    # When p^2 is greater than n, it is no longer a possible factor
+    # This is because the root of n is the upper limit of its factors as shown by perfect squares
+    # E.g. 7 is the largest factor of 49
+    while p * p <= n: 
+        if n % p == 0: # If it is a factor (has no remainder)
+            largestPrime = p
+            while n % p == 0: # Divide out the factor then set n to the quotient
+                n = n / p
+        # Loop through our 'primes', just 2 plus odd numbers: 2,3,5,7,9,11...
+        # This is not the most performant due to it testing against odd non primes
+        if p == 2:
+            p = 3
+        else:
+            p = p + 2
+    # Since we divided out all numbers less than the root,
+    # Anything left in n must be a prime itself
+    if n != 1:
+        print n
+    # Otherwise, n is 1 meaning we divided out a prime so print the largest prime we found
+    else:
+        print largestPrime
